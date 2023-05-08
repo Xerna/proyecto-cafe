cc0cedf0ff7a41f9c8782384ec7aabda8d2c7f52
Xerna <ricardo.cerna2001@gmail.com>
2023-05-07 22:27:40 -0600

    Cambios realizados al header y al index

---
+++ public/includes/header.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="es">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Proyecto Cafe</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+  
+  <script src="https://kit.fontawesome.com/ee2c3492f4.js" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="public/css/styles.css">
+</head>
+
+<body class="bg-main">
+  <header>
+    <nav class="navbar bg-brown-secondary bg-brown d-flex justify-content-evenly navbar navbar-expand">
+          <a class="navbar-brand d-flex align-items-center justify-content-center " href="#" styles="heigth=10%;">
+            <img src="public/img/logo.png" alt="Bootstrap" width="70">
+            <h3 class="mt-3">The Coffee Shop</h3>
+          </a>
+          <div class="navbar-nav border border-main p-1">
+              <a class="nav-link link   " href="#">Editar Menu</a>
+              <a class="nav-link link   border-main" href="#">Editar Usuarios</a>
+              <a class="nav-link link   border-main" href="#">Ver Ordenes</a>
+          </div>
+          <div class="bg-test" id="user-logged" styles="heigth=100%;">
+          <h6 class="fs-6 text-white px-3 py-2"><i class="fa-solid fa-user me-2"></i>Ricardo Cerna</h6> 
+          </div>
+    </nav>
+  </header>
--- test.php
+++
@@ -1,9 +0,0 @@
-<?php
-    try {
-        $conn = new PDO('mysql:host=localhost;dbname=ufg100420', 'root','03312001');
-    } catch (PDOException $e) {
-        print "Error!: " . $e->getMessage() . "<br/>";
-        die();
-    }
-    echo("HOLA");
-?>
